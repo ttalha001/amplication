42aa0c7817a5f80aa41e01c7f8650cf56717c73e
Amit Barletz <amit@amplication.com>
2023-09-08 14:37:59 +0300

    feat(client): wip

--- packages/amplication-client/src/Resource/resourceSettings/codeGeneratorVersion/CodeGeneratorVersionForm.tsx
+++ packages/amplication-client/src/Resource/resourceSettings/codeGeneratorVersion/CodeGeneratorVersionForm.tsx
@@ -1,91 +1,185 @@
-import { useContext } from "react";
+import { useCallback, useContext, useState } from "react";
 import * as models from "../.././../models";
 import "./CodeGeneratorVersionForm.scss";
 import { AppContext } from "../../../context/appContext";
 import { Form, Formik } from "formik";
 import { validate } from "../../../util/formikValidateJsonSchema";
 import { useTracking } from "../../../util/analytics";
-import useSettingsHook from "../../useSettingsHook";
 import FormikAutoSave from "../../../util/formikAutoSave";
-import useResource from "../../hooks/useResource";
 import {
   CodeGeneratorVersionStrategy,
   strategyConfiguration,
 } from "./CodeGeneratorVersionStrategy";
+import { useMutation } from "@apollo/client";
+import { UPDATE_CODE_GENERATOR_VERSION } from "./queries";
+import { AnalyticsEventNames } from "../../../util/analytics-events.types";
 
 const CLASS_NAME = "code-generator-service-form";
 
+type CodeGeneratorVersionOptions = {
+  codeGeneratorVersion: string;
+  codeGeneratorStrategy: models.CodeGeneratorVersionStrategy;
+};
+
+const CODE_GENERATOR_FORM_SCHEMA = {
+  properties: {
+    codeGeneratorVersion: {
+      type: "string",
+    },
+    codeGeneratorStrategy: {
+      type: models.CodeGeneratorVersionStrategy,
+    },
+  },
+  required: ["codeGeneratorStrategy"],
+};
+
 function CodeGeneratorVersionForm() {
   const { currentResource } = useContext(AppContext);
-  const { resourceSettings, updateResourceSettings } = useResource(
-    currentResource.id
-  );
+  const [codeGeneratorVersionOptions, setCodeGeneratorVersionOptions] =
+    useState<CodeGeneratorVersionOptions>({
+      codeGeneratorVersion: null,
+      codeGeneratorStrategy: models.CodeGeneratorVersionStrategy.LatestMajor,
+    });
+  const resourceId = currentResource?.id;
 
   const { trackEvent } = useTracking();
 
-  const resourceId = currentResource?.id;
-  const { handleSubmit, SERVICE_CONFIG_FORM_SCHEMA } = useSettingsHook({
-    trackEvent,
-    resourceId,
-    updateResourceSettings,
+  const [updateCodeGeneratorVersion] = useMutation<{
+    data: {
+      codeGeneratorOptions: {
+        codeGeneratorVersion: string;
+        codeGeneratorStrategy: models.CodeGeneratorVersionStrategy;
+      };
+    };
+    where: {
+      id: string;
+    };
+  }>(UPDATE_CODE_GENERATOR_VERSION, {
+    onCompleted: () => {
+      trackEvent({
+        eventName: AnalyticsEventNames.CodeGeneratorVersionUpdate,
+        properties: {
+          strategy: codeGeneratorVersionOptions.codeGeneratorStrategy,
+          version: codeGeneratorVersionOptions.codeGeneratorVersion,
+        },
+      });
+    },
   });
+
+  const handleSubmit = useCallback((data) => {
+    console.log("handleSubmit", data);
+    updateCodeGeneratorVersion({
+      variables: {
+        data: {
+          codeGeneratorVersionOptions: {
+            codeGeneratorStrategy: data.codeGeneratorStrategy,
+            codeGeneratorVersion: data.codeGeneratorVersion || null,
+          },
+        },
+        where: {
+          id: resourceId,
+        },
+      },
+    });
+  }, []);
 
   const handleStrategyChange = (
     strategy: models.CodeGeneratorVersionStrategy
   ) => {
     console.log("handleStrategyChange", strategy);
+    updateCodeGeneratorVersion({
+      variables: {
+        data: {
+          codeGeneratorVersionOptions: {
+            codeGeneratorStrategy: strategy,
+          },
+        },
+        where: {
+          id: resourceId,
+        },
+      },
+    });
   };
 
   const handleVersionSelect = (version: string) => {
     console.log("handleVersionSelect", version);
+    updateCodeGeneratorVersion({
+      variables: {
+        data: {
+          codeGeneratorVersionOptions: {
+            codeGeneratorStrategy:
+              codeGeneratorVersionOptions.codeGeneratorStrategy,
+            codeGeneratorVersion: version,
+          },
+        },
+        where: {
+          id: resourceId,
+        },
+      },
+    });
   };
 
   return (
     <div className={CLASS_NAME}>
-      {resourceSettings?.serviceSettings && (
-        <Formik
-          initialValues={resourceSettings.serviceSettings}
-          validate={(values: models.ServiceSettings) =>
-            validate(values, SERVICE_CONFIG_FORM_SCHEMA)
+      <Formik
+        initialValues={{
+          CodeGeneratorVersionStrategy:
+            models.CodeGeneratorVersionStrategy.LatestMajor,
+        }}
+        validate={(
+          values: Pick<
+            models.Resource,
+            "codeGeneratorStrategy" | "codeGeneratorVersion"
+          >
+        ) => {
+          const errors = {};
+          validate(values, CODE_GENERATOR_FORM_SCHEMA);
+          if (
+            values.codeGeneratorStrategy !==
+              models.CodeGeneratorVersionStrategy.LatestMajor &&
+            !values.codeGeneratorVersion
+          ) {
+            errors["codeGeneratorVersion"] = "Required";
           }
-          enableReinitialize
-          onSubmit={handleSubmit}
-        >
-          {(formik) => {
-            return (
-              <Form>
-                <div className={`${CLASS_NAME}__header`}>
-                  <h3>Choose Code Generator Version Settings</h3>
-                  <h5>Select one of the version settings options:</h5>
-                </div>
-                {strategyConfiguration.map(
-                  ({
-                    strategy,
-                    title,
-                    description,
-                    selectTitle,
-                    selectPlaceholder,
-                    iconName,
-                  }) => (
-                    <CodeGeneratorVersionStrategy
-                      savedStrategy={strategy}
-                      onStrategyChange={handleStrategyChange}
-                      onVersionSelect={handleVersionSelect}
-                      strategy={strategy}
-                      title={title}
-                      description={description}
-                      selectTitle={selectTitle}
-                      selectPlaceholder={selectPlaceholder}
-                      iconName={iconName}
-                    />
-                  )
-                )}
-                <FormikAutoSave debounceMS={200} />
-              </Form>
-            );
-          }}
-        </Formik>
-      )}
+          return errors;
+        }}
+        enableReinitialize
+        onSubmit={handleSubmit}
+      >
+        {(formik) => {
+          return (
+            <Form>
+              <div className={`${CLASS_NAME}__header`}>
+                <h3>Choose Code Generator Version Settings</h3>
+                <h5>Select one of the version settings options:</h5>
+              </div>
+              {strategyConfiguration.map(
+                ({
+                  strategy,
+                  title,
+                  description,
+                  selectTitle,
+                  selectPlaceholder,
+                  iconName,
+                }) => (
+                  <CodeGeneratorVersionStrategy
+                    savedStrategy={strategy}
+                    onStrategyChange={handleStrategyChange}
+                    onVersionSelect={handleVersionSelect}
+                    strategy={strategy}
+                    title={title}
+                    description={description}
+                    selectTitle={selectTitle}
+                    selectPlaceholder={selectPlaceholder}
+                    iconName={iconName}
+                  />
+                )
+              )}
+              <FormikAutoSave debounceMS={200} />
+            </Form>
+          );
+        }}
+      </Formik>
     </div>
   );
 }
